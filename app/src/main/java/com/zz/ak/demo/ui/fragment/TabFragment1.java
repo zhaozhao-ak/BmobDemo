eeeef7848bf67defc092e5360b476213efca6806
460644290@qq.com <zhao11810953>
2017-11-08 00:21:58 +0800

    添加通讯录页面，侧边栏快捷搜索

--- app/src/main/java/com/zz/ak/demo/ui/fragment/TabFragment1.java
+++ app/src/main/java/com/zz/ak/demo/ui/fragment/TabFragment1.java
@@ -1,25 +1,160 @@
 package com.zz.ak.demo.ui.fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersAdapter;
+import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersDecoration;
 import com.zz.ak.demo.R;
+import com.zz.ak.demo.tool.bar.CityListAdapter;
+import com.zz.ak.demo.tool.bar.DividerDecoration;
+import com.zz.ak.demo.tool.bar.constants.DataConstants;
+import com.zz.ak.demo.tool.bar.model.City;
+import com.zz.ak.demo.tool.bar.unit.QuickSideBarTipsView;
+import com.zz.ak.demo.tool.bar.unit.QuickSideBarView;
+import com.zz.ak.demo.tool.bar.unit.listener.OnQuickSideBarTouchListener;
+
+import java.lang.reflect.Type;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
  * Created by chengxi on 17/4/26.
  */
-public class TabFragment1 extends Fragment {
+public class TabFragment1 extends Fragment implements OnQuickSideBarTouchListener {
+
+    RecyclerView recyclerView;
+    HashMap<String,Integer> letters = new HashMap<>();
+    QuickSideBarView quickSideBarView;
+    QuickSideBarTipsView quickSideBarTipsView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-       return inflater.inflate(R.layout.fragment_tab_1, null);
+       View view =  inflater.inflate(R.layout.fragment_tab_1, null);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        quickSideBarView = (QuickSideBarView) view.findViewById(R.id.quickSideBarView);
+        quickSideBarTipsView = (QuickSideBarTipsView) view.findViewById(R.id.quickSideBarTipsView);
+
+        //设置监听
+        quickSideBarView.setOnQuickSideBarTouchListener(this);
+
+        //设置列表数据和浮动header
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Add the sticky headers decoration
+        CityListWithHeadersAdapter adapter = new CityListWithHeadersAdapter();
+
+        //GSON解释出来
+        Type listType = new TypeToken<LinkedList<City>>(){}.getType();
+        Gson gson = new Gson();
+        LinkedList<City> cities = gson.fromJson(DataConstants.cityDataList, listType);
+
+        ArrayList<String> customLetters = new ArrayList<>();
+
+        int position = 0;
+        for(City city: cities){
+            String letter = city.getFirstLetter();
+            //如果没有这个key则加入并把位置也加入
+            if(!letters.containsKey(letter)){
+                letters.put(letter,position);
+                customLetters.add(letter);
+            }
+            position++;
+        }
+
+        //不自定义则默认26个字母
+        quickSideBarView.setLetters(customLetters);
+        adapter.addAll(cities);
+        recyclerView.setAdapter(adapter);
+
+        final StickyRecyclerHeadersDecoration headersDecor = new StickyRecyclerHeadersDecoration(adapter);
+        recyclerView.addItemDecoration(headersDecor);
+
+        // Add decoration for dividers between list items
+        recyclerView.addItemDecoration(new DividerDecoration(this.getContext()));
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+    }
+
+    @Override
+    public void onLetterChanged(String letter, int position, float y) {
+        quickSideBarTipsView.setText(letter, position, y);
+        //有此key则获取位置并滚动到该位置
+        if(letters.containsKey(letter)) {
+            recyclerView.scrollToPosition(letters.get(letter));
+        }
+    }
+
+    @Override
+    public void onLetterTouching(boolean touching) {
+        //可以自己加入动画效果渐显渐隐
+        quickSideBarTipsView.setVisibility(touching? View.VISIBLE:View.INVISIBLE);
+    }
+
+    private class CityListWithHeadersAdapter extends CityListAdapter<RecyclerView.ViewHolder>
+            implements StickyRecyclerHeadersAdapter<RecyclerView.ViewHolder> {
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.view_item, parent, false);
+            return new RecyclerView.ViewHolder(view) {
+            };
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            TextView textView = (TextView) holder.itemView;
+            textView.setText(getItem(position).getCityName());
+        }
+
+        @Override
+        public long getHeaderId(int position) {
+            return getItem(position).getFirstLetter().charAt(0);
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.view_header, parent, false);
+            return new RecyclerView.ViewHolder(view) {
+            };
+        }
+
+        @Override
+        public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int position) {
+            TextView textView = (TextView) holder.itemView;
+            textView.setText(String.valueOf(getItem(position).getFirstLetter()));
+//            holder.itemView.setBackgroundColor(getRandomColor());
+        }
+
+        private int getRandomColor() {
+            SecureRandom rgen = new SecureRandom();
+            return Color.HSVToColor(150, new float[]{
+                    rgen.nextInt(359), 1, 1
+            });
+        }
 
     }
 }
